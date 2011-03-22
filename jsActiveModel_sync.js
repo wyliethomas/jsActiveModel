007b452dd3ffa20563ab5f8ef27efe19e31d9685
Wylie Thomas <wylie@Wylie-Thomass-MacBook-Pro.local>
2011-03-22 17:19:21 -0600

    fixed callbacks for all and create

--- jsActiveModel_sync.js
+++ jsActiveModel_sync.js
@@ -13,19 +13,6 @@ JSActiveModelSync.parms = {
 JSActiveModelSync.prototype.init = function(parms){
   JSActiveModelSync.prototype.dbsetup(parms);
 }
-JSActiveModelSync.prototype.dbopen = function(parms){
-  //these vars should have been set on some global var
-  if(window.openDatabase){
-    var DATABASE = parms['DATABASE'];
-    var DBVERSION = parms['DBVERSION'];
-    var DBDESCRIPTION = parms['DBDESCRIPTION'];
-    var DBSIZE = parms['DBSIZE'];
-    db = openDatabase(DATABASE, DBVERSION, DBDESCRIPTION, DBSIZE);
-    return true;
-  }else{
-    return false;
-  }
-}
 JSActiveModelSync.prototype.dbsetup = function(parms){
   if(JSActiveModelSync.prototype.dbopen(parms)){
     var DBTABLE = parms['DBTABLE'];
@@ -39,11 +26,34 @@ JSActiveModelSync.prototype.dbsetup = function(parms){
     //methods.get_records( url );
   }
 }
-JSActiveModelSync.prototype.update = function(){
+JSActiveModelSync.prototype.dbopen = function(parms){
+  //these vars should have been set on some global var
+  if(window.openDatabase){
+    var DATABASE = " "+parms['DATABASE']+" ";
+    var DBVERSION = parms['DBVERSION'];
+    var DBDESCRIPTION = " "+parms['DBDESCRIPTION']+" ";
+    var DBSIZE = parms['DBSIZE'];
+    db = openDatabase(DATABASE, DBVERSION, DBDESCRIPTION, DBSIZE);
+    return true;
+  }else{
+    return false;
+  }
 }
-JSActiveModelSync.prototype.create = function(){
-}
-JSActiveModelSync.prototype.destroy = function(){
+JSActiveModelSync.prototype.auto_increment = function(DBTABLE, auto_handle){
+  function queryHandle(tx){
+    tx.executeSql('SELECT MAX(local_storage_id) AS last_local_id FROM ' + DBTABLE, [], querySuccess, errorHandle );
+  }
+  function querySuccess(tx, results){
+    for (var i=0; i<results.rows.length; i++) {
+      var row = results.rows.item(i);
+      last_local_id = row['last_local_id'] + 1;
+    }
+    auto_handle(last_local_id);
+  }
+  function errorHandle(err){
+    handler(err);
+  }
+  db.transaction(queryHandle, errorHandle);
 }
 
 
@@ -53,47 +63,64 @@ JSActiveModelSync.prototype.destroy = function(){
 
 //klass methods
 JSActiveModelSync.klass = {
-  all : function(){
-    JSActiveModelSync.prototype.init(this.parms); //make sure db is available and open
+  all : function(handler){
     DBTABLE = this.parms['DBTABLE'];
-    db.transaction(function (tx) {
-      tx.executeSql('SELECT * FROM ' + DBTABLE, [], successHandler, errorHandler);
-    });
-    successHandler = function(tx, results){
-      if(results.rows.length == 0){
-        console.log('got no rows');
-        data = '0';
-          //insert data because the table's empty
-      } else {
-        console.log('got some rows');
-        data = [];
-        var len = results.rows.length, i;
-        for (i = 0; i < len; i++) {
-          //data.push({DBTABLE:{'name': results.rows.item(i).name}}); 
-        }
+    JSActiveModelSync.prototype.init(this.parms); //make sure db is available and open
+
+    function queryHandle(tx){
+      tx.executeSql('SELECT * FROM ' + DBTABLE, [], querySuccess, errorHandle );
+    }
+
+    function querySuccess(tx, results){
+      if(typeof(success) == 'function'){
+        handler(results.rows);
       }
-    };
-    errorHandler = function(tx, results){
-      console.log('got an error');
-    };
+    }
+
+    function errorHandle(err){
+      handler(err);
+    }
+
+    db.transaction(queryHandle, errorHandle);
   },
   find : function(){
-    db.transaction(function (tx) {
-      tx.executeSql('SELECT * FROM '+ DBTABLE +' WHERE local_storage_id = ?', [id], function (tx, results) {
-        data = [];
-        var len = results.rows.length, i;
-        for (i = 0; i < len; i++) {
-          data.push({DBTABLE:{'name': results.rows.item(i).name}}); 
-          //alert(results.rows.item(i).name);
-        }
-        findHandle(data);
-      });
-    });
-  },
-  where : function(){
   },
   find_by_sql : function(){
-  }
+  },
+  update : function(){
+    DBTABLE = this.parms['DBTABLE'];
+    console.log('this is update');
+  },//end update
+  create : function(post_data, handler){
+    DBTABLE = this.parms['DBTABLE'];
+    DBCOLUMNS = this.parms['DBCOLUMNS'];
+    JSActiveModelSync.prototype.auto_increment(DBTABLE, function(auto_handle){
+      function insertHandle(tx, results){
+        //prep post_data to have right values for right columns
+        data = [];
+        for(column in DBCOLUMNS){
+         if(DBCOLUMNS[column] in post_data){
+          data.push('"' + post_data[DBCOLUMNS[column]] + '"');
+         }else{
+          data.push("null");
+         }
+        }
+        keys = DBCOLUMNS + ', local_storage_id';
+        values = data + ', ' + auto_handle;
+        tx.executeSql('INSERT INTO ' + DBTABLE + '(' + keys + ') VALUES (' + values + ')' );
+        tx.executeSql('INSERT INTO ' + DBTABLE + '_AUDIT(' + keys + ') VALUES (' + values + ')' );
+      }
+      function successHandle(){
+        //trigger sync here?
+      }
+      function errorHandle(err){
+        handler(err);
+      }
+      db.transaction(insertHandle, errorHandle, successHandle);
+    }); //auto increment before creating the record
+  },//end create
+  destroy : function(){
+  }//end destroy
 }
 
 
